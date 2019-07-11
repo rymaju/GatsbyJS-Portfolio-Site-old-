53e59f7aca233f5dafbb20780e139d19b6f83ad3
Ryan Jung <ryan.matthew.jung@gmail.com>
2019-07-11 21:34:42 +0800

    Index page Anime.js animation rendered onComponentDidMount()
    Signed-off-by: Ryan Jung <ryan.matthew.jung@gmail.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import BlogLayout from "../components/blogLayout";
+import SEO from "../components/seo";
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>Error 404</h1>
-    <p>Oops, the file you requested does not exist. ;-;</p>
-  </Layout>
-)
+    <BlogLayout>
+        <SEO title="404: Not found" />
+        <h1>Error 404</h1>
+        <p>Oops, the file you requested does not exist. ;-;</p>
+    </BlogLayout>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,44 +1,43 @@
-import React from 'react';
+import React from "react";
 import BlogLayout from "../../components/blogLayout";
-import { graphql, Link } from 'gatsby';
+import { graphql, Link } from "gatsby";
+import SEO from "../../components/seo";
 
 //Blog index page that will display my blog posts sorted by date
 //you will be able to click on the posts and read them too!
 
-const BlogIndex = ({data}) => {
-    return(
+const BlogIndex = ({ data }) => {
+    return (
         <BlogLayout>
+            <SEO title="Blog" />
             <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-            {data.allMarkdownRemark.edges.map(({node}) => (
-                <div key = {node.id}>
-                    <Link
-                        to={node.fields.slug}
-                    >
-                        <h2>{node.frontmatter.title} - {node.frontmatter.date}</h2>
-                    
-                        
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+                <div key={node.id}>
+                    <Link to={node.fields.slug}>
+                        <h2>
+                            {node.frontmatter.title} - {node.frontmatter.date}
+                        </h2>
                     </Link>
-                    <p>
-                        {node.excerpt.slice(0,250)}
-                    </p>
+                    <p>{node.excerpt.slice(0, 250)}</p>
                 </div>
             ))}
         </BlogLayout>
     );
-}
+};
 
 export default BlogIndex;
 
 export const query = graphql`
-    query{
-        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
+    query {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
             totalCount
             edges {
                 node {
                     id
                     frontmatter {
                         title
-                        date(formatString: "MMMM DD, YYYY")                    }
+                        date(formatString: "MMMM DD, YYYY")
+                    }
                     fields {
                         slug
                     }
