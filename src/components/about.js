4905cb3cf307f5f31c79a652ae3e0c60c86887e0
Ryan Jung <ryan.matthew.jung@gmail.com>
2019-10-26 12:20:22 -0400

    minor landing css changes

--- src/components/about.js
+++ src/components/about.js
@@ -5,14 +5,17 @@ const About = () => {
         <div id="about" className={styles.skillsContainer}>
             <h1>About Me</h1>
             <p>
-                Hi, my name is Ryan and I'm a software engineer specializing in
-                website development. I have 4 years of computer science
-                experience and am continuing to learn computer science at
-                Northeastern University. I have a broad range of skills, which
-                you can view below. The vast majority of my projects have a
-                ReactJS frontend, but I have used many different technologies
-                for the backend. I can learn new technologies very quickly and I
-                have tangible projects to prove my expertise.
+                <b>
+                    Hi, my name is Ryan and I'm a software engineer specializing
+                    in website development.
+                </b>{" "}
+                I have 4+ years of computer science experience and am continuing
+                to learn computer science at Northeastern University. I have a
+                broad range of skills, which you can view below. The vast
+                majority of my projects have a ReactJS frontend, but I have used
+                many different technologies for the backend. I can learn new
+                technologies very quickly and I have tangible projects to prove
+                my expertise.
             </p>
             <p>
                 Most importantly, I'm passionate about software and I love to
